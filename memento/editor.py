1a07b1a8a6bec7cddbb18b1ccc1aebea737dc3d6
Jerzy <jgrynczewski@gmail.com>
2020-05-23 14:09:34 +0200

    Memento v1

--- memento/editor.py
+++ memento/editor.py
@@ -1,6 +1,7 @@
 class Editor:
     def __init__(self):
         self.__content = ""
+        self.prev_content = []
 
     @property
     def content(self):
@@ -8,11 +9,17 @@ class Editor:
 
     @content.setter
     def content(self, value):
+        self.prev_content.append(self.__content)
         self.__content = value
+
+    def undo(self):
+        self.__content = self.prev_content.pop()
+
 
 e = Editor()
 e.content = "a"
 e.content = "b"
 e.content = "c"
+e.undo()
 e.undo()
 print(e.content)
